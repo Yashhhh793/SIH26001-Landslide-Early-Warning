6241783bb983969f9302718e70fad7a21afd9d53
Yashhhh793 <yashkamble9095@gmail.com>
2026-09-25 22:25:42 +0530

    Fix oscillator stop time in app.py

--- app.py
+++ app.py
@@ -2324,7 +2324,7 @@ if (window.landslideAudioContext) {
     oscillator.start();
 
     oscillator.stop(
-        ctx.currentTime + 5
+        ctx.currentTime + 10
     );
 }
                            new Notification(
